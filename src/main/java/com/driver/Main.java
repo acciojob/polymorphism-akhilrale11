70c257d8af41fc1d0ec69c715b420743de1ebfba
akhilrale11 <117888290+akhilrale11@users.noreply.github.com>
2024-11-08 13:01:55 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,34 +1,25 @@
 package com.driver;
-
 public class Main {
+    public static void main(String[] args) {
 
-    // Task 1: Create a class Product inside Main class
-    static class Product {
+        Product p = new Product();
+        System.out.println( p.product(10, 3));
+        System.out.println(p.product(10, 3, 7));
+        System.out.println(p.product(10.4, 8.4));
 
-        // Task 3: Create a method with the following definition
+    }
+
+    public static class Product {
         public int product(int x, int y) {
             return x * y;
         }
 
-        // Task 4: Create an overloaded method with the following definition
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        // Task 5: Create an overloaded method with the following definition
         public double product(double x, double y) {
             return x * y;
         }
-    }
 
-    public static void main(String[] args) {
-
-        // Task 2: Create an object of Product in Main function called p
-        Product p = new Product();
-
-        // Call the methods and print results
-        System.out.println("Product of 2 integers (3, 4): " + p.product(3, 4));
-        System.out.println("Product of 3 integers (2, 3, 4): " + p.product(2, 3, 4));
-        System.out.println("Product of 2 doubles (2.5, 4.5): " + p.product(2.5, 4.5));
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
     }
 }
